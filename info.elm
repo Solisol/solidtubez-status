a936b41cefac193d0b78850c5c2f75e5433acde5
Jessica Andersson <ja@speedledger.se>
2016-05-04 08:16:59 +0200

    elm init

---
+++ info.elm
@@ -0,0 +1,6 @@
+import Html exposing (text)
+
+main =
+  text "Information about solidtubez.com:
+The server will be down from 5/5-2016 and undefined time space due to change of server location.
+Information will be available here, when possible."
