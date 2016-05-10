186f55b1b2939106fb178e07ab04406b642a38b6
Jessica Andersson <ja@speedledger.se>
2016-05-10 09:31:20 +0200

    Updated server information

--- info.elm
+++ info.elm
@@ -4,6 +4,6 @@ main : Element
 main =
   flow down
     [ show "Information about solidtubez.com:"
-    , show "The server will be down from 5/5-2016 and undefined time space due to change of server location."
-    , show "Information will be available here, when possible."
+    , show "The server is up and running. No known issues"
+    , show "No planned downtime."
     ]
